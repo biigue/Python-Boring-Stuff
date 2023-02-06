81f75e69d816459263adb02ae1b87c53d22f2826
Bianca <=>
2023-02-06 10:07:43 -0300

    clean

--- trads/index.py
+++ trads/index.py
@@ -5,11 +5,8 @@ pt = open('trads/pt.json', 'w')
 es = open('trads/es.json', 'w')
 en = open('trads/en.json', 'w')
 
-
-def endof():
-    pt.write('},\n')
-    es.write('},\n')
-    en.write('},\n')
+file = open('trads/entrada.csv', 'r')
+items = file.readlines()
 
 
 def setAll(val):
@@ -21,35 +18,43 @@ def setAll(val):
     en.write('{\n')
 
 
-with open('c:\\Users\\bianc\\Pictures\\Python-Boring-Stuff\\trads\\entrada.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            pt.write('{\n')
-            es.write('{\n')
-            en.write('{\n')
-            line_count += 1
+def endof():
+    pt.write('}\n')
+    es.write('}\n')
+    en.write('}\n')
+
+
+for i in range(0, len(items)):
+    if i > 0:
+        text = items[i].replace('\n', '').split(';')
+        if text[1] == '' and text[2] == '':
+            if i > 1:
+                pt.write('},\n')
+                es.write('},\n')
+                en.write('},\n')
+            setAll(text[0])
         else:
-            if row[1] == '' and row[2] == '':
-                setAll(row[0])
-            else:
-                pt.write(f'\t"{row[0]}": "{row[1]}",\n')
-                en.write(f'\t"{row[0]}": "{row[2]}",\n')
-                es.write(f'\t"{row[0]}": "{row[3]}",\n')
-# pt.write(f'</ul>\n')
-# es.write(f'</ul>\n')
-# en.write(f'</ul>\n')
+            try:
+                next = items[i+1].replace('\n', '').split(';')
+                if next[1] == '' and next[2] == '':
+                    pt.write(f'\t"{text[0]}": "{text[1]}"\n')
+                    en.write(f'\t"{text[0]}": "{text[2]}"\n')
+                    es.write(f'\t"{text[0]}": "{text[3]}"\n')
+                else:
+                    pt.write(f'\t"{text[0]}": "{text[1]}",\n')
+                    en.write(f'\t"{text[0]}": "{text[2]}",\n')
+                    es.write(f'\t"{text[0]}": "{text[3]}",\n')
+            except:
+                pt.write(f'\t"{text[0]}": "{text[1]}"\n')
+                en.write(f'\t"{text[0]}": "{text[2]}"\n')
+                es.write(f'\t"{text[0]}": "{text[3]}"\n')
+                endof()
+    else:
+        pt.write('{\n')
+        es.write('{\n')
+        en.write('{\n')
 endof()
+
 pt.close
 en.close
 es.close
-
-
-# cwd = os.getcwd()  # Get the current working directory (cwd)
-# files = os.listdir(cwd)  # Get all the files in that directory
-# print("Files in %r: %s" % (cwd, files))
-
-# f = open("trads/pt.json", "w")
-# f.write("\nSee you soon!x")
-# f.close()
